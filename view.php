7790417724749999e29c17282c1d11903d5979f7
Rune Heidtmann <37456552+runeheidtmann@users.noreply.github.com>
2019-01-12 10:51:39 +0100

    Update view.php

--- view.php
+++ view.php
@@ -11,7 +11,9 @@
  * 
  * Make sure to put the excact amount of placeholders that will be needed. 
  * Whilst the script will ignore any key-value-pairs in excess, placeholders in excess will not be ignored.
- * and if not replaced, the output will include placeholders like  {{placeholder_in_excess}}
+ * and if not replaced, the output will include placeholders like  {{placeholder_in_excess}}.
+ *
+ * Also make sure that your placeholders is named the same as the keys. Otherwise they will not be rendered.
  * 
  * It wil take a lot more logic for the script to count and ignore placeholders in excess.
  */ 
